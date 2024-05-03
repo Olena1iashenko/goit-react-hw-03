f1eae09a60bdcfdeeae4a50dbbe59e0e668168fb
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2024-05-03 18:35:28 +0300

    base settings

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
